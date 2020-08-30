989bb4c93eb2ba7d0206cfbdf0af842aff7ca579
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 21:31:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_to_dennis
         }
      }
       programmer_hash[:dennis_ritchie] = {}
-programmer_hash[:dennis_ritchie][:languages].include?("Assembly")
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 programmer_hash
 end
 
